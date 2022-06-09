acac7106c672499fc391b93720c0ea20111c43f3
ddophi98 <lightcloud98@gmail.com>
2022-06-09 13:51:51 +0900

    fix: 이미지 이름 변경

--- GilCat/GilCat/Sources/Global/Components/CustomTitle.swift
+++ GilCat/GilCat/Sources/Global/Components/CustomTitle.swift
@@ -7,23 +7,23 @@ struct CustomTitle: View {
         return HStack {
             ZStack {
                 if titleText.count == 1 {
-                    Image("Polygon1").offset(x: 11, y: 11).opacity(0.85)
+                    Image("oneLetter").offset(x: 11, y: 11).opacity(0.85)
                         
                     Text("\(titleText)").bold().foregroundColor(.white).font(.system(size: 40)).padding()
                 } else if titleText.count == 2 {
-                    Image("Polygon2").offset(x: 11, y: 11).opacity(0.85)
+                    Image("twoLetter").offset(x: 11, y: 11).opacity(0.85)
                         
                     Text("\(titleText)").bold().foregroundColor(.white).font(.system(size: 40)).padding()
                 } else if titleText.count == 3 {
-                    Image("Polygon3").offset(x: 11, y: 11).opacity(0.85)
+                    Image("threeLetter").offset(x: 11, y: 11).opacity(0.85)
                         
                     Text("\(titleText)").bold().foregroundColor(.white).font(.system(size: 40)).padding()
                 } else if titleText.count == 4 {
-                    Image("Polygon4").offset(x: 11, y: 11).opacity(0.85)
+                    Image("fourLetter").offset(x: 11, y: 11).opacity(0.85)
                         
                     Text("\(titleText)").bold().foregroundColor(.white).font(.system(size: 40)).padding()
                 } else if titleText.count == 5 {
-                    Image("Polygon5").offset(x: 11, y: 11).opacity(0.85)
+                    Image("fiveLetter").offset(x: 11, y: 11).opacity(0.85)
                         
                     Text("\(titleText)").bold().foregroundColor(.white).font(.system(size: 40)).padding()
                 } else if titleText.count > 5 {
