ab597df476424175e51adafb110f9fe1af66f00a
ddophi98 <lightcloud98@gmail.com>
2022-06-09 13:50:09 +0900

    feat: 이미지 그리드 형태로 나오게 하기

--- GilCat/GilCat/Sources/Screen/Registers/registerViews/register8.swift
+++ GilCat/GilCat/Sources/Screen/Registers/registerViews/register8.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct Register8: View {
+    let catImages = [
+        "cat_gray_1",
+        "cat_gray_2",
+        "cat_gray_3",
+        "cat_gray_4",
+        "cat_gray_5",
+        "cat_gray_6",
+        "cat_gray_7",
+        "cat_gray_8"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            CustomTitle(titleText: "캐릭터")
+            Spacer()
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(0..<catImages.count/2, id: \.self) { index in
+                            VStack {
+                                Image(catImages[2*index])
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100)
+                                Image(catImages[2*index+1])
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100)
+                            }
+                        
+                    }
+                }
+                .padding()
+                .background()
+            }
+            Button {
+                
+            } label: {
+                CustomMainButton(text: "다음", foreground: Color.white, background: Color.lightOrange)
+            }
+        }
+        .padding()
+        .background(Color.backgroundColor)
     }
 }
 
