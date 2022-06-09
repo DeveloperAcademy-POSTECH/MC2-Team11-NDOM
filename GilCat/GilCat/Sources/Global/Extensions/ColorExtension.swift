aa0b3bb454d5fa883ad3a86c68175675f80b4e4c
ddophi98 <lightcloud98@gmail.com>
2022-06-09 13:49:11 +0900

    fix: 이름 중복되는 문제때문에 수정

--- GilCat/GilCat/Sources/Global/Extensions/ColorExtension.swift
+++ GilCat/GilCat/Sources/Global/Extensions/ColorExtension.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 extension Color {
     static let backgroundColor = Color("BackGroundColor")
-    static let orange = Color("ButtonColor")
+    static let lightOrange = Color("ButtonColor")
     static let darkBlue = Color("DarkBlueColor")
+    static let darkGray = Color("GrayColor")
 }
