db21bafeec908848bea65bc6b4abdcf24d01759f
ddophi98 <lightcloud98@gmail.com>
2022-06-13 21:31:00 +0900

    fix: 코드 입력을 하는 텍스트 필드를 6개에서 1개로 줄임

--- GilCat/GilCat/Sources/Screen/Registers/registerViews/RegisterCode.swift
+++ GilCat/GilCat/Sources/Screen/Registers/registerViews/RegisterCode.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct RegisterCode: View {
     @EnvironmentObject var catInfo: GilCatInfoList
-    @FocusState var focusedCode: Int?
+    @FocusState var isFocused: Bool?
     @Binding var buildNavigationStack: Bool
     @State var isLinkActive = false
     @State var isAlertActice = false
-    @State var codeInput = ["", "", "", "", "", ""]
+    @State var codeInput = ""
     
     init(_ buildNavigationStack: Binding<Bool>) {
         UITextView.appearance().backgroundColor = .clear
@@ -31,14 +31,24 @@ struct RegisterCode: View {
                 Spacer()
             }
             // 코드 입력 칸
-            HStack {
-                getCodeInputView(index: 0)
-                getCodeInputView(index: 1)
-                getCodeInputView(index: 2)
-                Spacer()
-                getCodeInputView(index: 3)
-                getCodeInputView(index: 4)
-                getCodeInputView(index: 5)
+            ZStack {
+                TextField("", text: $codeInput)
+                    .frame(width: 0, height: 0)
+                    .focused($isFocused, equals: true)
+                    .keyboardType(.numberPad)
+                    .onChange(of: codeInput) { _ in
+                        if codeInput.count > 6 {
+                            codeInput = String(codeInput.prefix(6))
+                        }
+                    }
+                HStack {
+                    getCodeInputView(index: 0)
+                    getCodeInputView(index: 1)
+                    getCodeInputView(index: 2)
+                    getCodeInputView(index: 3)
+                    getCodeInputView(index: 4)
+                    getCodeInputView(index: 5)
+                }
             }
             .padding()
             Spacer()
@@ -53,10 +63,9 @@ struct RegisterCode: View {
                     Button {
                         // Todo: 코드에 따라 서버에서 다른 고양이 룸 정보 받아오기
                         // 코드가 다 입력이 안됐다면, 팝업 창 보여주기
-                        for code in codeInput where code.isEmpty {
+                        if codeInput.count != 6 {
                             isAlertActice = true
-                        }
-                        if !isAlertActice {
+                        } else {
                             isLinkActive = true
                         }
                     } label: {
@@ -75,33 +84,26 @@ struct RegisterCode: View {
         .onAppear {
             // 화면이 나타나고 0.5초 뒤에 자동으로 공유코드 첫번째 입력칸에 포커스 되도록 하기
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    focusedCode = 0
+                    isFocused = true
             }
         }
     }
     
     // 코드 입력 칸 반환하기
     func getCodeInputView(index: Int) -> some View {
-        return TextEditor(text: $codeInput[index])
+        var codes = ["", "", "", "", "", ""]
+        for (idx, code) in codeInput.enumerated() {
+            if codeInput.count > 6 {
+                break
+            }
+            codes[idx] = String(code)
+        }
+        return Text(codes[index])
             .frame(width: 50, height: 65)
             .background(Color.pickerColor)
             .cornerRadius(20)
-            .multilineTextAlignment(.center)
             .foregroundColor(Color.white)
             .font(.system(size: 40, weight: Font.Weight.heavy))
-            .keyboardType(.numberPad)
-            .focused($focusedCode, equals: index)
-            .onChange(of: codeInput[index]) { _ in
-                if codeInput[index].count > 1 {
-                    codeInput[index] = String(codeInput[index].suffix(1))
-                }
-                if focusedCode != nil {
-                    focusedCode! += 1
-                    if focusedCode == 5 {
-                        focusedCode = nil
-                    }
-                }
-            }
     }
 }
 
