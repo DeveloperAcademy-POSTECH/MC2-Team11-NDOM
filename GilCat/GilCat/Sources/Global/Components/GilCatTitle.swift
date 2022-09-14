93f5b3f7169c810222301a844838c0408220a05d
ddophi98 <lightcloud98@gmail.com>
2022-09-14 14:45:48 +0900

    얼룩무늬 삭제

--- GilCat/GilCat/Sources/Global/Components/GilCatTitle.swift
+++ GilCat/GilCat/Sources/Global/Components/GilCatTitle.swift
@@ -9,37 +9,8 @@ struct GilCatTitle: View {
      
      */
     func customTitle(titleText: String) -> some View {
-        var imageName = ""
-        var imageWidth = 0
-        // 글자의 길이에 따라 어떤 얼룩 무늬로 할지, 얼룩 무늬 이미지의 크기는 얼마나 할지 정하기
-        if titleText.count == 1 {
-            imageName = "oneLetter"
-            imageWidth = 30
-        } else if titleText.count == 2 {
-            imageName = "twoLetter"
-            imageWidth = 60
-        } else if titleText.count == 3 {
-            imageName = "threeLetter"
-            imageWidth = 90
-        } else if titleText.count == 4 {
-            imageName = "fourLetter"
-            imageWidth = 120
-        } else if titleText.count == 5 {
-            imageName = "fiveLetter"
-            imageWidth = 150
-        } else if titleText.count > 5 {
-            imageName = "twoLine"
-            imageWidth = 200
-        }
         return HStack {
-            ZStack {
-                Image(imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: ContentMode.fit)
-                    .offset(x: 11, y: 11).opacity(0.85)
-                    .frame(width: CGFloat(imageWidth))
-                Text("\(titleText)").bold().foregroundColor(.white).font(.system(size: 40)).padding()
-            }
+            Text("\(titleText)").bold().foregroundColor(.white).font(.system(size: 40)).padding()
         }
     }
     var body: some View {
